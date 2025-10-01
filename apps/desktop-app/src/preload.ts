31892cb212956531bdc4a568b2e2409d2a574960
Trieu Tran <trieut@bu.edu>
2025-10-01 16:59:53 -0400

    Frontend ADDITIONSSS, github and google login and redirect

--- apps/desktop-app/src/preload.ts
+++ apps/desktop-app/src/preload.ts
@@ -1,6 +1,8 @@
+// src/preload.ts
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("electron", {
-  login: () => ipcRenderer.invoke("oauth-login"),
-  loginGoogle: () => ipcRenderer.invoke("oauth-login-google"),
+  login: () => ipcRenderer.invoke("oauth-login"),          // GitHub
+  loginGoogle: () => ipcRenderer.invoke("oauth-login-google"), // Google
 });
+
